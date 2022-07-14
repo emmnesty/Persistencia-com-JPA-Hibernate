00a41bad4142541e1efd90dbb95da6d102a6944c
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-14 15:21:28 -0300

    Consultas com filtros

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -28,14 +28,21 @@ public class ProdutoDao {
 		categoria = em.merge(categoria);
 		this.em.remove(categoria);
 	}
-	
-	public Produto buscarPorId (Long id) {
+
+	public Produto buscarPorId(Long id) {
 		return em.find(Produto.class, id);
 	}
-	
+
 	public List<Produto> buscarTodos() {
 		String jpql = "SELECT p FROM Produto p";
 		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+
+	public List<Produto> buscarPorNome(String nome) {
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
 	}
 
 }
