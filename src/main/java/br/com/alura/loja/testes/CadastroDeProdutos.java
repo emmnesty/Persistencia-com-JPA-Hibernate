940689e4ac1920f0c5da01dc4ba3e786ad42555c
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-14 13:21:09 -0300

    Configurando o projeto, e criando a conexão com JPA

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -0,0 +1,28 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class CadastroDeProdutos {
+
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+		celular.setNome("Xiaomi Redmi");
+		celular.setDescricao("Celular lindo sqn");
+		celular.setPreco(new BigDecimal("800"));
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);
+		
+		em.getTransaction().begin();
+		dao.cadastrar(celular);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
