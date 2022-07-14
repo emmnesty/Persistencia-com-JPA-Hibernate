497dc082a229a45fe4ec2c1ef67b2399d77a1605
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-14 14:17:00 -0300

    Anotações JPA, Relacionamento de tabelas

--- src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -4,23 +4,29 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("Celular lindo sqn");
-		celular.setPreco(new BigDecimal("800"));
+		Categoria celulares = new Categoria("CELULARES");
+		
+		
+		Produto celular = new Produto("Xiaomi Redmi", "Celular lindo sqn", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
